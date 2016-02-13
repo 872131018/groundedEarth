1473df5019d290041d0621aff9cffe2a1ad2b5ec
872131018 <j-huffman@hotmail.com>
2016-02-12 19:33:39 -0700

    allow click for color select

--- js/editorDriver.js
+++ js/editorDriver.js
@@ -1,51 +1,68 @@
 $(document).ready(function()
 {
+  /*
+  * Track the color that was clicked on as a Global
+  */
+  color = '';
+  /*
+  * Delegate all clicks to the document
+  */
   $(document).on('click', function(event)
   {
-    var color = '';
-
-    $(".circle").css("height", "60px");
-    $(".circle").css("width", "60px");
-    switch(event.target.id)
+    /*
+    * Isolate element that was clicked on
+    */
+    var $target = $(event.target);
+    /*
+    * Determine general course of action based on class
+    */
+    if($target.hasClass("circle"))
     {
-      case 'circle1':
-        $("#circle1").css("height", "90px");
-        $("#circle1").css("width", "90px");
-        color = "green";
-        $('#feathers').attr("fill", color);
-        break;
-      case 'circle2':
-        $("#circle2").css("height", "90px");
-        $("#circle2").css("width", "90px");
-        color = "red";
-        $('#feathers').attr("fill", color);
-        break;
-      case 'circle3':
-        $("#circle3").css("height", "90px");
-        $("#circle3").css("width", "90px");
-        color = "blue";
-        $('#feathers').attr("fill", color);
-        break;
-      default:
-        break;
-    }
-
-    var parents = $(event.target).parents();
-    for(var parent in parents)
-    {
-      $parent = $(parents[parent]);
-      if(typeof $parent.attr("id") != 'undefined')
+      /*
+      * Reset the size of the circles to neutral state
+      */
+      $(".circle").css("height", "60px");
+      $(".circle").css("width", "60px");
+      /*
+      * Switch on id to set color
+      */
+      switch($target.attr("id"))
       {
-        console.log(color)
-        switch($parent.attr("id"))
+        case 'circle1':
+          color = "green";
+          break;
+        case 'circle2':
+          color = "red";
+          break;
+        case 'circle3':
+          color = "blue";
+          break;
+        default:
+          break;
+      }
+      /*
+      * Add a selection indicator to the selected element
+      */
+      $target.css("height", "90px");
+      $target.css("width", "90px");
+    }
+    else
+    {
+      /*
+      * Climb up the dom tree for an id
+      */
+      var parents = $target.parents();
+      for(var $parent in parents)
+      {
+        $parent = $(parents[$parent]);
+        /*
+        * Only selectable layers should have an id
+        */
+        if(typeof $parent.attr("id") != 'undefined')
         {
-          case 'feathers':
-            $('#feathers').attr("fill", color);
-            break
-          default:
-            break;
+          $parent.attr("fill", color);
+          break;
         }
-        break;
       }
     }
   });
