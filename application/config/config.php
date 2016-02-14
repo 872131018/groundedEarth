5b80ba0d1ef0d3e9a7d152b744eb137d217d2ca8
872131018 <j-huffman@hotmail.com>
2016-02-13 19:17:14 -0700

    removed index page config

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://73.243.194.169/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
