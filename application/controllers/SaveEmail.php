bafb6517a3e4219e17f76e2b29d077b0438c9fdf
872131018 <j-huffman@hotmail.com>
2016-02-25 19:56:14 -0700

    removed parameter

--- application/controllers/SaveEmail.php
+++ application/controllers/SaveEmail.php
@@ -17,7 +17,7 @@ class SaveEmail extends CI_Controller
 		*Each key of $data will be available as variable in view
 		*TODO: Convert to using an object rather than an array
 		*/
-		$result = $this->email_factory->saveEmail($passedEmail, $passedType);
+		$result = $this->email_factory->saveEmail($passedEmail);
 		/*
 		* Set content type to json and send result
 		*/
