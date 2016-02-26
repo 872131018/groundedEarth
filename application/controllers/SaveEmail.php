cd74d42be29be8f74f3b7d167018698d2d30aad8
872131018 <j-huffman@hotmail.com>
2016-02-25 19:58:27 -0700

    asdf

--- application/controllers/SaveEmail.php
+++ application/controllers/SaveEmail.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SaveEmail extends CI_Controller
 {
-	public function construct()
+	public function __construct()
   {
     parent::construct();
 		/*
