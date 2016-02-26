a8b0b44253aefb1bb18b740b26c779dd17efed34
872131018 <j-huffman@hotmail.com>
2016-02-25 21:01:35 -0700

    changes

--- application/controllers/SaveEmail.php
+++ application/controllers/SaveEmail.php
@@ -3,19 +3,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class SaveEmail extends CI_Controller
 {
+	function __construct()
+	{
+		/*
+    * The factory is where you put the models to work and manages them
+    */
+		parent::__construct();
+    $this->load->library("email_factory");
+	}
 	public function index()
 	{
 		/*
-    *The factory is where you put the models to work and manages them
-    */
-    $this->load->library("email_factory");
-		/*
-		*Each key of $data will be available as variable in view
-		*TODO: Convert to using an object rather than an array
+		* Pull the email from the request
 		*/
 		$email = $this->input->post("email");
-		$this->email_factory->saveEmail($email);
-		$result = $this->email_factory->saveEmail();
+		/*
+		* Send email to factory to be saved
+		*/
+		$result = $this->email_factory->saveEmail($email);
+		/*
+		* White list result
+		*/
+		$result = ($result ? "true" : "false");
 		/*
 		* Set content type to json and send result
 		*/
