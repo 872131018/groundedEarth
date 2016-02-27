52c7619aca9a5a0848b2756bbc3d51fd73ea1ee8
872131018 <j-huffman@hotmail.com>
2016-02-27 08:45:36 -0700

    file name changes

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class SaveEmail extends CI_Controller
+class Email extends CI_Controller
 {
 	public function __construct()
 	{
