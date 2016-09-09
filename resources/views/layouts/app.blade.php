63b048be427c51947646e713d50a24aa45199bc8
John Huffman <johnhuffman@Johns-MacBook-Pro.local>
2016-09-09 15:53:38 -0600

    updates to file structure, removed some legacy stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.css" rel="stylesheet">
-    <link href="{{ @getenv("APP_URL") }}css/styles_admin.css" rel="stylesheet">
+    <link href="{{ @getenv("APP_URL") }}stylesheets/styles_admin.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -88,15 +88,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.js"></script>
     <script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-    <script src="{{ @getenv("APP_URL") }}js/index_admin.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_product_add.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_product_edit.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_product_submit.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_product_delete.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_file_delete.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_content_add.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_content_edit.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_content_submit.js"></script>
-    <script src="{{ @getenv("APP_URL") }}js/delegate_content_delete.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/index_admin.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_product_add.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_product_edit.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_product_submit.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_product_delete.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_file_delete.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_content_add.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_content_edit.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_content_submit.js"></script>
+    <script src="{{ @getenv("APP_URL") }}javascripts/delegate_content_delete.js"></script>
 </body>
 </html>
